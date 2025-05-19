630215cc99f9d9b24356aa635c5ea8cc6c5b2fc9
beto <robertoneivacorvino@gmail.com>
2025-05-19 20:31:58 -0300

    att

--- src/Property.java
+++ src/Property.java
@@ -1,9 +1,37 @@
 package src;
 
 public class Property {
-    private int sell;
+    private int price;
     private int rent;
     private Player owner;
 
-    public void Property(int sell, int rent){}
+    public Property(int price, int rent) {
+        this.price = price;
+        this.rent = rent;
+        this.owner = null;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public boolean haveOwner() {
+        return owner != null;
+    }
+
+    public void resetOwner() {
+            owner = null;
+    }
 }
