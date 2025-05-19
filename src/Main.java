9ae48a45187e2f9b04e3f87bd0263c08bf81cb20
Vinicius Paes Berna <viniciuspaesberna@gmail.com>
2025-05-19 20:02:15 -0300

    feat: create player class base

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Bankrupt");
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        window.setVisible(true);
+    }
+}
