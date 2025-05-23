8eb6367812b4b6d4fb80d46f7247d5b9cae0148d
Assad <guilhermeassad5@gmail.com>
2025-05-23 11:18:07 -0700

    ATT

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.run();
+
     }
 }
